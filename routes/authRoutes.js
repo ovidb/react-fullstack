a0acff55d4b81b6f5ddb558145090f571e21c963
Ovidiu D Bodea <antarexis@gmail.com>
2017-09-05 22:21:00 +0100

    Logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,5 +12,10 @@ module.exports = app => {
 
   app.get('/api/current-user', (req, res) => {
     res.send(req.user);
-  })
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };
